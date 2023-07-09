140a8bb125fc31259dd6bfa66d30da608512aca2
Baik Gwangin <gwangin1999@naver.com>
2023-07-09 18:57:22 +0900

    feat: 이미지의 가로 혹은 세로 길이를 300px로 설정

--- src/middlewares/resizeImage.ts
+++ src/middlewares/resizeImage.ts
@@ -11,10 +11,13 @@ const midResizeImage = async (
   res: Response,
   next: NextFunction,
 ) => {
-  // 요청에 파일이 없다면 다음 미들웨어로 통과시킨다.
+  // NOTE : 요청에 파일이 없다면 다음 미들웨어로 통과시킨다.
   if (!req.files) next();
 
   const imageFiles = req.files as Express.Multer.File[];
+
+  // NOTE : 가로 혹은 세로의 길이를 300px 로 고정시킨다. (비례 축소)
+  const MAX_LENGTH = 300; 
   try {
     await Promise.all(
       imageFiles.map(async (imageFile) => {
@@ -23,7 +26,7 @@ const midResizeImage = async (
         const originFileName = imageFile.originalname.split(".")[0]
         const { width, height } = await sharp(fileContent).metadata();
         const [smallerWidth, smallerHeight] = [width, height].map((length) =>
-          Math.round(Math.min(length ?? 500, 500)),
+          Math.round(Math.min(length ?? MAX_LENGTH, MAX_LENGTH)),
         );
 
         // NOTE : sharp 라이브러리를 활용하여 확장자를 webp로 변환 및 사이즈 축소
