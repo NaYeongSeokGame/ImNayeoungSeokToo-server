5a3497447b0c9adbbaab9ff86e7db74c62f3909b
RookieAND <gwangin1999@naver.com>
2023-08-25 17:13:48 +0900

    feat: util validation 원시 값 Casting 로직 추가

--- src/validations/util.validation.ts
+++ src/validations/util.validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const paginatedSchema = z.object({
-  page: z.number().min(1).default(1).optional(),
-  limit: z.number().min(1).default(9).optional(),
+  page: z.coerce.number().positive().default(1),
+  limit: z.coerce.number().positive().default(9),
 });
