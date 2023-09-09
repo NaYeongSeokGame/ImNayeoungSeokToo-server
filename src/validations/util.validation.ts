11bc9fa413d160396a4135b4f2c2383cc2921884
Baik Gwangin <gwangin1999@naver.com>
2023-09-10 04:18:19 +0900

    hotfix: 퀴즈 생성 관련 Validation Schema 수정

--- src/validations/util.validation.ts
+++ src/validations/util.validation.ts
@@ -4,3 +4,7 @@ export const paginatedSchema = z.object({
   page: z.coerce.number().positive().default(1),
   limit: z.coerce.number().positive().default(9),
 });
+
+export const hashtagSchema = z.string().min(3).max(10);
+
+export const presetPinSchema = z.string().regex(/[A-Z0-9]{6}/);
