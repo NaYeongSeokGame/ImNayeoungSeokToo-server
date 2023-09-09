496baa07848665168d9d7f9387184a040fdeb9d7
Baik Gwangin <gwangin1999@naver.com>
2023-09-10 02:33:49 +0900

    feat: 파일 업로드 관련 Validation 로직 추가

--- src/configs/multerConfig.ts
+++ src/configs/multerConfig.ts
@@ -4,6 +4,12 @@ import multer from 'multer';
 type FileNameCallbackFunc = (error: Error | null, filename: string) => void;
 
 /**
+ * 파일 갯수 제한 : 9개, 파일 당 사이즈 제한 : UNDER 10MB
+ */
+const MAX_COUNT = 9;
+const MAX_SIZE = 1024 * 1024 * 10;
+
+/**
  * Express의 Request body 에 존재하는 파일을 읽어 저장하도록 하는 Config
  * destination 옵션에 정의된 경로에 파일을 저장하며, callback을 통해 파일의 이름으로 저장한다.
  */
@@ -18,4 +24,15 @@ export const multerConfig: multer.Options = {
       callback(null, file.originalname);
     },
   }),
+  limits: {
+    fieldSize: MAX_SIZE,
+    fields: MAX_COUNT,
+  },
+  // sharp 에서 지원하는 파일 확장자 (jpeg, png, webp, avif, tiff) 만 허용
+  fileFilter: (req, file, callback) => {
+    callback(
+      null,
+      file.mimetype.match(/(image\/(jpeg|png|webp|avif|tiff))/)?.length !== 0,
+    );
+  },
 };
